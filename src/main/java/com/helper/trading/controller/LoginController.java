3ee2adca5eb48bafe9ea1e9227f22609b8a78ee5
Satoshi Nakamoto <cup.cake@inbox.ru>
2017-07-27 14:04:46 +0300

    Request mapping for login controller

--- src/main/java/com/helper/trading/controller/LoginController.java
+++ src/main/java/com/helper/trading/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/app")
 public class LoginController {
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView simplePage() {
