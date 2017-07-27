d6e49c80901b44ffc16038e7278739c10a07f951
Satoshi Nakamoto <cup.cake@inbox.ru>
2017-07-27 18:02:16 +0300

    WebConfig edit

--- src/main/java/com/helper/trading/application/configuration/web/WebConfig.java
+++ src/main/java/com/helper/trading/application/configuration/web/WebConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
         super.addResourceHandlers(registry);
     }
 }
