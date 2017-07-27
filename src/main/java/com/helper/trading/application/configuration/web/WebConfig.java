119c91d49ca837f22bc49209e278f683c8a21d29
Satoshi Nakamoto <cup.cake@inbox.ru>
2017-07-27 15:33:14 +0300

    WebConfig

--- src/main/java/com/helper/trading/application/configuration/web/WebConfig.java
+++ src/main/java/com/helper/trading/application/configuration/web/WebConfig.java
@@ -9,16 +9,16 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-/*@Configuration
+@Configuration
 @EnableWebMvc
-@EnableSpringDataWebSupport*/
+@EnableSpringDataWebSupport
 public class WebConfig extends WebMvcConfigurationSupport {
-    /*@Bean
+    @Bean
     public ViewResolver getViewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setPrefix("/jsp/");
         return resolver;
-    }*/
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
