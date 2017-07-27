0d64a0c617a985c3d6a96fdc9d287238d150d1bc
Satoshi Nakamoto <cup.cake@inbox.ru>
2017-07-27 15:21:39 +0300

    ResponseBody

--- src/main/java/com/helper/trading/controller/TestController.java
+++ src/main/java/com/helper/trading/controller/TestController.java
@@ -2,11 +2,13 @@ package com.helper.trading.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
     @RequestMapping("/")
+    @ResponseBody
     public String test() {
         return "Hi!";
     }
