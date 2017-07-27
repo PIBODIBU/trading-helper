ad215396470475b83798de57a2f82c9fd63326e9
Satoshi Nakamoto <cup.cake@inbox.ru>
2017-07-27 12:23:45 +0300

    Deploy db settings (Heroku)

--- src/main/java/com/helper/trading/application/configuration/security/AuthenticationProviderConfig.java
+++ src/main/java/com/helper/trading/application/configuration/security/AuthenticationProviderConfig.java
@@ -10,9 +10,9 @@ public class AuthenticationProviderConfig {
     public DriverManagerDataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/tradinghelper");
-        dataSource.setUsername("root");
-        dataSource.setPassword("");
+        dataSource.setUrl("jdbc:mysql://us-cdbr-iron-east-03.cleardb.net/heroku_0deeb3e187c3980");
+        dataSource.setUsername("b9ee1f875e9873");
+        dataSource.setPassword("dde5ca57");
         return dataSource;
     }
 }
