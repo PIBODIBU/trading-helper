50cb91cf6cbc5bcabb2e90abeaf436b70d7a8a66
Satoshi Nakamoto <cup.cake@inbox.ru>
2017-07-27 13:20:35 +0300

    Spring Boot @ComponentScan added

--- src/main/java/com/helper/trading/application/Application.java
+++ src/main/java/com/helper/trading/application/Application.java
@@ -3,6 +3,7 @@ package com.helper.trading.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EntityScan(basePackages = {"com.helper.trading.model"})
 @EnableJpaRepositories(basePackages = {"com.helper.trading.repository"}, transactionManagerRef = "txManager")
 @EnableTransactionManagement
+@ComponentScan("com.helper.trading")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
