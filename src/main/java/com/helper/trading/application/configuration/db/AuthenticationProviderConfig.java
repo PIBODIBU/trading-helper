712f14434856f06c26453a928bab756b404eea77
Satoshi Nakamoto <cup.cake@inbox.ru>
2017-07-27 13:27:31 +0300

    WebConfig improvement

--- src/main/java/com/helper/trading/application/configuration/db/AuthenticationProviderConfig.java
+++ src/main/java/com/helper/trading/application/configuration/db/AuthenticationProviderConfig.java
@@ -1,4 +1,4 @@
-package com.helper.trading.application.configuration.security;
+package com.helper.trading.application.configuration.db;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ src/main/java/com/helper/trading/application/configuration/web/WebConfig.java
@@ -0,0 +1,30 @@
+package com.helper.trading.application.configuration.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@EnableSpringDataWebSupport
+public class WebConfig extends WebMvcConfigurationSupport {
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/jsp/");
+        return resolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/resources/**")) {
+            registry.addResourceHandler("/resources/**").addResourceLocations(
+                    "/static/");
+        }
+    }
+}
