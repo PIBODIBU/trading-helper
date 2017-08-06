6488bbcd147238fc47fd7a365ad64586ba385427
Satoshi Nakamoto <cup.cake@inbox.ru>
2017-08-06 18:35:38 +0300

    APICurrencyRateController improved

--- src/main/java/com/helper/trading/application/configuration/db/AuthenticationProviderConfig.java
+++ src/main/java/com/helper/trading/application/configuration/db/AuthenticationProviderConfig.java
@@ -8,11 +8,18 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 public class AuthenticationProviderConfig {
     @Bean(name = "SystemDataSource")
     public DriverManagerDataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        /*DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://us-cdbr-iron-east-03.cleardb.net/heroku_0deeb3e187c3980");
         dataSource.setUsername("b9ee1f875e9873");
-        dataSource.setPassword("dde5ca57");
+        dataSource.setPassword("dde5ca57");*/
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://81.162.228.39/tradinghelper");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+
         return dataSource;
     }
 }
